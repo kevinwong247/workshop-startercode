ae48bb53373bdd49f0b724bc2f0e794559fbe613
kevin <kevinwong247@gmail.com>
2018-09-04 17:33:53 -0400

    let me push

---
+++ classroom.rb
@@ -0,0 +1,27 @@
+require './student'
+require './grade'
+
+class Classroom
+  attr_accessor :name
+  attr_accessor :students
+
+  def initialize(name)
+    @name = name
+    @students = {}
+  end
+
+  def add_student(student)
+    if @students[student.name]
+      raise "Sorry, #{student} is already in here."
+    end
+    @students[student.name] = student
+  end
+
+  def roster
+    puts @name + " roster:"
+    @students.each do |key, value|
+      puts "#{key}: #{value.age}"
+    end
+  end
+
+end
---
+++ grade.rb
@@ -0,0 +1,11 @@
+class Grade
+  attr_accessor :subject, :score
+
+  def initialize(subject, score)
+    @subject = subject
+    @score = score
+  end
+
+end
+
+
